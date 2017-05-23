ff06761886f24a789f9c203f1c88bce1c8dc4204
Begimai Amantaeva <begimai.amantaeva@gmail.com>
2017-05-23 23:51:13 +0600

    Merge sort algorithm in Java

--- MergeSort.java
+++ MergeSort.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class mergeSort {
+public class MergeSort {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
