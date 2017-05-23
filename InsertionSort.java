06bed80fce7f0589157fe9b9aee31d9453c2a88b
Begimai Amantaeva <begimai.amantaeva@gmail.com>
2017-05-23 23:50:28 +0600

    Insertion Sort algorithm in Java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class insertionSort {
+public class InsertionSort {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
